66a2f0875ea8b3b99a969b5bbfab749004cfac4e
Anton Karpov <slam.caponezed@mail.ru>
2025-07-25 11:03:35 +0300

    Some test stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AfterViewInit, Component, ElementRef, TemplateRef, viewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [FormsModule, FormsModule],
+  template: `
+  <div class="d-flex flex-column gap-2 p-3">
+    <div #template>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores veniam ab ipsam? Blanditiis deserunt quas odit est, voluptas accusamus totam sit id. Atque assumenda hic earum perferendis consectetur nostrum sapiente!</p>
+    </div>
+  </div>
+  `,
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'angular-test';
+export class AppComponent implements AfterViewInit {
+
+  template = viewChild('template', { read: ElementRef });
+
+  ngAfterViewInit(): void {
+    console.log(this.template());
+  }
 }
